40812f1c1968fee0cea7ac7fc4c7c8e8cb375a4f
Mohammad Mohammadi <emprism@gmail.com>
2020-04-12 19:40:20 +0430

    Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-162860125-1`,
       },
     },
     {
